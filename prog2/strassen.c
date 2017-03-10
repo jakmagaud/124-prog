9802dff08d8b811ecaff69dbf6cca53d5a7d84b8
jakmagaud <jakmagaud@college.harvard.edu>
2017-03-10 01:07:05 -0500

    prog2 skeleton code

---
+++ prog2/strassen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct matrix {
+
+} matrix;
+
+matrix* mat_mult(matrix matr1, matrix matr2) {
+	return NULL;
+}
+
+matrix* strassen_mult(matrix matr1, matrix matr2) {
+	return NULL;
+}
+
+int main(int argc, char** argv) {
+	//flag options: 0 for no extra stuff
+	int flag = atoi(argv[1]);
+	int dim = atoi(argv[2]);
+	char* fname = argv[3];
+}
