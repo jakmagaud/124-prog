5fa419522a8e83200abb1012ed7e939b038e6c45
jakmagaud <jakmagaud@college.harvard.edu>
2017-03-14 18:23:20 -0400

    added vectors

--- prog2/strassen.c
+++ prog2/strassen.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
+#include "strassen.h"
 
-typedef struct matrix {
+vector* create_vector(int len) {
+	vector* vec = malloc(sizeof(vector));
+	if (len < INIT_VEC_LEN)
+		len = INIT_VEC_LEN;
+	vec->buf = malloc(len * sizeof(int));
+	vec->len = 0;
+	vec->max_len = len;
+	return vec;
+}
 
-} matrix;
+void free_vector(vector* vec) {
+	assert(vec != NULL);
+	if (vec->buf != NULL)
+		free(vec->buf);
+	free(vec);
+}
+
+int vector_insert(vector* vec, int i) {
+	if (vec->len >= vec->max_len) {
+		vec->max_len *= 2;
+		vec->buf = realloc(vec->buf, vec->max_len * sizeof(int));
+	}
+	vec->buf[vec->len] = i;
+	vec->len++;
+	return vec->len - 1;
+}
 
 matrix* mat_mult(matrix matr1, matrix matr2) {
 	return NULL;
