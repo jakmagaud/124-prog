691d1f79b465112ce1671e42ceb0567ec02fba0b
jakmagaud <jakmagaud@college.harvard.edu>
2017-03-16 16:08:08 -0400

    wrote and tested naive matmult as well as mat addition

--- prog2/strassen.c
+++ prog2/strassen.c
@@ -4,44 +4,75 @@
 #include <assert.h>
 #include "strassen.h"
 
-vector* create_vector(int len) {
-	vector* vec = malloc(sizeof(vector));
-	if (len < INIT_VEC_LEN)
-		len = INIT_VEC_LEN;
-	vec->buf = malloc(len * sizeof(int));
-	vec->len = 0;
-	vec->max_len = len;
-	return vec;
+matrix* mat_init(int rows, int cols) {
+	matrix* mat = malloc(sizeof(matrix));
+	mat->rows = rows;
+	mat->cols = cols;
+	mat->data = calloc(1, sizeof(int) * rows * cols);
+	return mat;
 }
 
-void free_vector(vector* vec) {
-	assert(vec != NULL);
-	if (vec->buf != NULL)
-		free(vec->buf);
-	free(vec);
+int mat_index(matrix mat, int r, int c) {
+	int index = r * mat.cols + c;
+	assert(index < mat.rows * mat.cols);
+	return index;
 }
 
-int vector_insert(vector* vec, int i) {
-	if (vec->len >= vec->max_len) {
-		vec->max_len *= 2;
-		vec->buf = realloc(vec->buf, vec->max_len * sizeof(int));
+matrix* mat_add(matrix mat1, matrix mat2, bool sub) {
+	assert(mat1.rows == mat2.rows && mat1.cols == mat2.cols);
+	matrix* result = mat_init(mat1.rows, mat1.cols);
+
+	for (int i = 0; i < mat1.rows; i++) {
+		for (int j = 0; j < mat1.cols; j++) {
+			int ind = mat_index(mat1, i, j);
+			if (!sub)
+				result->data[ind] = mat1.data[ind] + mat2.data[ind];
+			else
+				result->data[ind] = mat1.data[ind] - mat2.data[ind];
+		}
 	}
-	vec->buf[vec->len] = i;
-	vec->len++;
-	return vec->len - 1;
+	return result;
 }
 
-matrix* mat_mult(matrix matr1, matrix matr2) {
+matrix* mat_pad(matrix mat) {
 	return NULL;
 }
 
-matrix* strassen_mult(matrix matr1, matrix matr2) {
+matrix* mat_mult(matrix mat1, matrix mat2) {
+	assert(mat1.cols == mat2.rows);
+	matrix* result = mat_init(mat1.rows, mat2.cols);
+	for (int i = 0; i < mat1.rows; i++) {
+		for (int j = 0; j < mat2.cols; j++) {
+			for (int k = 0; k < mat1.cols; k++) {
+				int ind1 = mat_index(*result, i, j);
+				int ind2 = mat_index(mat1, i, k);
+				int ind3 = mat_index(mat2, k, j);
+				result->data[ind1] += mat1.data[ind2] * mat2.data[ind3];
+			}
+		}
+	}
+	return result;
+}
+
+matrix* strassen_mult(matrix mat1, matrix mat2) {
 	return NULL;
 }
 
 int main(int argc, char** argv) {
 	//flag options: 0 for no extra stuff
-	int flag = atoi(argv[1]);
-	int dim = atoi(argv[2]);
-	char* fname = argv[3];
+	// int flag = atoi(argv[1]);
+	// int dim = atoi(argv[2]);
+	// char* fname = argv[3];
+
+	matrix* mat1 = mat_init(2,2);
+	matrix* mat2 = mat_init(2,2);
+	mat1->data[0] = 1;
+	mat1->data[1] = 2;
+	mat1->data[2] = 3;
+	mat1->data[3] = 4;
+	mat2->data[0] = 4;
+	mat2->data[1] = 3;
+	mat2->data[2] = 2;
+	mat2->data[3] = 1;
+	matrix* result = mat_mult(*mat1, *mat2);
 }
