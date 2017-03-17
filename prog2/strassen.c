6b87ff9f8da677a58ba291acf654cabcb405fd7f
jakmagaud <jakmagaud@college.harvard.edu>
2017-03-17 13:51:40 -0400

    matrix is now represented with 2d array

--- prog2/strassen.c
+++ prog2/strassen.c
@@ -8,14 +8,11 @@ matrix* mat_init(int rows, int cols) {
 	matrix* mat = malloc(sizeof(matrix));
 	mat->rows = rows;
 	mat->cols = cols;
-	mat->data = calloc(1, sizeof(int) * rows * cols);
+	mat->data = malloc(rows * sizeof(int*));
+	for (int i = 0; i < rows; i++) {
+		mat->data[i] = calloc(1, cols * sizeof(int));
+	}
 	return mat;
-}
-
-int mat_index(matrix mat, int r, int c) {
-	int index = r * mat.cols + c;
-	assert(index < mat.rows * mat.cols);
-	return index;
 }
 
 matrix* mat_add(matrix mat1, matrix mat2, bool sub) {
@@ -24,11 +21,10 @@ matrix* mat_add(matrix mat1, matrix mat2, bool sub) {
 
 	for (int i = 0; i < mat1.rows; i++) {
 		for (int j = 0; j < mat1.cols; j++) {
-			int ind = mat_index(mat1, i, j);
 			if (!sub)
-				result->data[ind] = mat1.data[ind] + mat2.data[ind];
+				result->data[i][j] = mat1.data[i][j] + mat2.data[i][j];
 			else
-				result->data[ind] = mat1.data[ind] - mat2.data[ind];
+				result->data[i][j] = mat1.data[i][j] - mat2.data[i][j];
 		}
 	}
 	return result;
@@ -44,10 +40,7 @@ matrix* mat_mult(matrix mat1, matrix mat2) {
 	for (int i = 0; i < mat1.rows; i++) {
 		for (int j = 0; j < mat2.cols; j++) {
 			for (int k = 0; k < mat1.cols; k++) {
-				int ind1 = mat_index(*result, i, j);
-				int ind2 = mat_index(mat1, i, k);
-				int ind3 = mat_index(mat2, k, j);
-				result->data[ind1] += mat1.data[ind2] * mat2.data[ind3];
+				result->data[i][j] += mat1.data[i][k] * mat2.data[k][j];
 			}
 		}
 	}
@@ -66,13 +59,13 @@ int main(int argc, char** argv) {
 
 	matrix* mat1 = mat_init(2,2);
 	matrix* mat2 = mat_init(2,2);
-	mat1->data[0] = 1;
-	mat1->data[1] = 2;
-	mat1->data[2] = 3;
-	mat1->data[3] = 4;
-	mat2->data[0] = 4;
-	mat2->data[1] = 3;
-	mat2->data[2] = 2;
-	mat2->data[3] = 1;
+	mat1->data[0][0] = 1;
+	mat1->data[0][1] = 2;
+	mat1->data[1][0] = 3;
+	mat1->data[1][1] = 4;
+	mat2->data[0][0] = 4;
+	mat2->data[0][1] = 3;
+	mat2->data[1][0] = 2;
+	mat2->data[1][1] = 1;
 	matrix* result = mat_mult(*mat1, *mat2);
 }
