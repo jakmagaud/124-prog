8c7c68b0301abb58f33599495c2bc15f2b7323d5
HSA Dev 4 <hsadev4@HSAs-iMac.local>
2017-04-16 23:29:29 -0400

    updated datagen.py

--- prog3/datagen.py
+++ prog3/datagen.py
@@ -1,9 +1,11 @@
 import random
+import sys
 
-f = open('nums.txt', 'w')
+for i in range(int(sys.argv[1])):
+	f = open('nums.txt' + str(i), 'w')
 
-for _ in range (100):
-	num = random.randint(1,pow(10,10))
-	f.write(str(num) + '\n')
+	for _ in range (100):
+		num = random.randint(1,pow(10,10))
+		f.write(str(num) + '\n')
 
-print "finished writing"
+	print "finished writing"
